39e0a47ab498d5204307db11a95e0051864a287e
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-09-09 18:17:11 +0300

    implemented firebase functions

--- README.md
+++ README.md
@@ -14,3 +14,16 @@ npm install
 npm run build
 npm run start-node
 ```
+
+## Deployment
+[Here](https://server-adaptive-loading-ect.firebaseapp.com)
+
+### deploy functions
+```
+npm run deploy-functions
+```
+
+### deploy app
+```
+npm run deploy-app
+```
--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,11 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send('Hello from Firebase!');
+// });
 
 const express = require('express');
 // const bodyParser = require('body-parser');
@@ -59,3 +67,5 @@ app.listen(
     console.log(`Frontend start on http://localhost:5000`);
   }
 );
+
+exports.app = functions.https.onRequest(app);
